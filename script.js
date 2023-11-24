1c01f28359df8bc875c6fe0da5abd9c19e4c28cb
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-24 09:23:18 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 //your JS code here. If required.
-// script.js
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
 
-document.getElementById('btn').addEventListener('click', async function() {
-  // Get user input
-  const textInput = document.getElementById('text').value;
-  const delayInput = document.getElementById('delay').value;
-
-  // Parse delay input to ensure it's a valid number
-  const delay = parseInt(delayInput, 10);
-
-  // Validate inputs
-  if (!textInput || isNaN(delay) || delay <= 0) {
-    alert('Please enter valid input for text and delay');
-    return;
-  }
-
-  // Delay using async/await
-  await new Promise(resolve => setTimeout(resolve, delay * 1000));
-
-  // Display message
-  const outputDiv = document.getElementById('output');
-  outputDiv.innerText = textInput;
+//your code here
+async function showMessage() {
+  const message = text.value;
+  const delayVal = delay.value;
+  await new Promise((resolve) => setTimeout(resolve, delayVal));
+  output.innerText = message;
+}
+showMessage().then(()=>{
+	output.innerHTML = text.value;
 });
+btn.addEventListener("click", showMessage);
